b474e4ca5180ede93b0effca262ed7f92cae5954
sladeda! <dawis.sl@gmail.com>
2018-10-15 15:30:48 +0200

    cviceni 03 - see TODO in MainFrame

--- src/cz/uhk/fim/rssreader/gui/MainFrame.java
+++ src/cz/uhk/fim/rssreader/gui/MainFrame.java
@@ -2,11 +2,15 @@ package cz.uhk.fim.rssreader.gui;
 
 
 
+import cz.uhk.fim.rssreader.utils.FileUtils;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class MainFrame extends JFrame {
-
 
     public MainFrame() {
         init();
@@ -17,9 +21,40 @@ public class MainFrame extends JFrame {
         setSize(1366,768);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+
+        initContentUI();
     }
 
-    //TODO kouknout na dva RSS feedy a zkopirovat si jejich url
-    //https://videacesky.cz/feed
-    //https://rss.idnes.cz
+
+    private void initContentUI(){
+        JPanel controlPanel = new JPanel(new BorderLayout());
+        JButton btnLoad = new JButton("Load");
+        JTextField txtInputField = new JTextField();
+        JButton btnSave = new JButton("Save");
+
+        controlPanel.add(btnLoad,"West");
+        controlPanel.add(btnSave,"East");
+        controlPanel.add(txtInputField,"Center");
+        add(controlPanel,"North");
+
+        JTextArea txtContent  = new JTextArea();
+        add(new JScrollPane(txtContent),"Center");
+
+        try {
+            txtContent.setText(FileUtils.loadStrinfFromFile("rssVC.xml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+        //TODO
+        //  add listeners for SAVE and LOAD
+        // pridat validateInput - true/false
+        // pridat JLabel ktery bude ukazovat chyby (vyskyt v kontrola panelu nad nebo pod txtInputFieldem)
+        //                  - mozne vystupy chyba pro load, save, validate (vzdy dat do catche) - enumerace
+
+
 }
---
+++ src/cz/uhk/fim/rssreader/model/RssItem.java
@@ -0,0 +1,52 @@
+package cz.uhk.fim.rssreader.model;
+
+public class RssItem {
+
+    private String title;
+    private String description;
+    private String link;
+    private String pubDate;
+    private String author;
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(String pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
